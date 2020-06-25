937c61ad1a0f5fde56aab595a87d37f39f4fa11a
yonghao <yyhao1@gmail.com>
2020-06-25 15:10:17 -0400

    basic stuff to setup db for janitor service

---
+++ src/main/java/bio/terra/janitor/app/StartupInitializer.java
@@ -0,0 +1,25 @@
+package bio.terra.janitor.app;
+
+import bio.terra.janitor.app.configuration.JanitorJdbcConfiguration;
+import bio.terra.janitor.service.migirate.MigrateService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+
+public final class StartupInitializer {
+  private static final Logger logger = LoggerFactory.getLogger(StartupInitializer.class);
+  private static final String changelogPath = "db/changelog.xml";
+
+  public static void initialize(ApplicationContext applicationContext) {
+    // Initialize or upgrade the database depending on the configuration
+    MigrateService migrateService = (MigrateService) applicationContext.getBean("migrateService");
+    JanitorJdbcConfiguration janitorJdbcConfiguration =
+        (JanitorJdbcConfiguration) applicationContext.getBean("janitorJdbcConfiguration");
+
+    if (janitorJdbcConfiguration.isInitializeOnStart()) {
+      migrateService.initialize(changelogPath, janitorJdbcConfiguration.getDataSource());
+    } else if (janitorJdbcConfiguration.isUpgradeOnStart()) {
+      migrateService.upgrade(changelogPath, janitorJdbcConfiguration.getDataSource());
+    }
+  }
+}
