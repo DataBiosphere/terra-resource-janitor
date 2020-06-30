2672b56699b1cf990b95a0c6f5dfc81cf9f81ebc
Yonghao Yu <yyhao1@gmail.com>
2020-06-30 13:18:20 -0400

    [CA-871] Initial DB setup using liquibase (#6)
    * basic stuff to setup db for janitor service
    
    * trigger a push
    
    * trigger a push
    
    * trigger a push
    
    * trigger a push
    
    * add missing dependency
    
    * reorder
    
    * reorder
    
    * reorder
    
    * Update src/main/resources/db/changesets/20200625_initial_schema.yaml
    
    Co-authored-by: Greg Polumbo <36171087+gpolumbo-broad@users.noreply.github.com>
    
    * Update src/main/resources/db/changesets/20200625_initial_schema.yaml
    
    Co-authored-by: Greg Polumbo <36171087+gpolumbo-broad@users.noreply.github.com>
    
    * Update src/main/java/bio/terra/janitor/service/migirate/MigrateService.java
    
    Co-authored-by: Greg Polumbo <36171087+gpolumbo-broad@users.noreply.github.com>
    
    * address comments
    
    * address comments
    
    * change  flag name
    
    Co-authored-by: Greg Polumbo <36171087+gpolumbo-broad@users.noreply.github.com>

---
+++ src/main/java/bio/terra/janitor/app/StartupInitializer.java
@@ -0,0 +1,26 @@
+package bio.terra.janitor.app;
+
+import bio.terra.janitor.app.configuration.JanitorJdbcConfiguration;
+import bio.terra.janitor.service.migirate.MigrateService;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Initializes the application after the application is setup, but before the port is opened for
+ * business. The purpose for this class is for database initialization and migration.
+ */
+public final class StartupInitializer {
+  private static final String changelogPath = "db/changelog.xml";
+
+  public static void initialize(ApplicationContext applicationContext) {
+    // Initialize or upgrade the database depending on the configuration
+    MigrateService migrateService = (MigrateService) applicationContext.getBean("migrateService");
+    JanitorJdbcConfiguration janitorJdbcConfiguration =
+        (JanitorJdbcConfiguration) applicationContext.getBean("janitorJdbcConfiguration");
+
+    if (janitorJdbcConfiguration.isRecreateDbOnStart()) {
+      migrateService.initialize(changelogPath, janitorJdbcConfiguration.getDataSource());
+    } else if (janitorJdbcConfiguration.isUpdateDbOnStart()) {
+      migrateService.upgrade(changelogPath, janitorJdbcConfiguration.getDataSource());
+    }
+  }
+}
