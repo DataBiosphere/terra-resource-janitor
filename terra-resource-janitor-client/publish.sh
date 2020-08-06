74188b070b166f03f782256b383d3529993bc2d2
yonghao <yyhao1@gmail.com>
2020-08-06 13:30:54 -0400

    pulish script

--- terra-resource-janitor-client/publish.sh
+++ terra-resource-janitor-client/publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Publish Janitor Client package:
+# Publish Janitor Client Package:
 VAULT_TOKEN=${1:-$(cat $HOME/.vault-token)}
 DSDE_TOOLBOX_DOCKER_IMAGE=broadinstitute/dsde-toolbox:consul-0.20.0
 ARTIFACTORY_ACCOUNT_PATH=secret/dsp/accts/artifactory/dsdejenkins
@@ -8,4 +8,5 @@ export ARTIFACTORY_USERNAME=$(docker run -e VAULT_TOKEN=$VAULT_TOKEN ${DSDE_TOOL
  vault read -field username ${ARTIFACTORY_ACCOUNT_PATH})
 export ARTIFACTORY_PASSWORD=$(docker run -e VAULT_TOKEN=$VAULT_TOKEN ${DSDE_TOOLBOX_DOCKER_IMAGE} \
  vault read -field password ${ARTIFACTORY_ACCOUNT_PATH})
+./gradlew test
 ./gradlew artifactoryPublish
