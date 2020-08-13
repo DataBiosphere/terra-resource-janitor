d32c0bc8432620eb6159107c35adfae91cdf4fb2
yonghao <yyhao1@gmail.com>
2020-08-13 15:40:03 -0400

    2nd draft

--- src/main/java/bio/terra/janitor/service/cleanup/flight/FlightMapKeys.java
+++ src/main/java/bio/terra/janitor/service/cleanup/flight/FlightMapKeys.java
@@ -5,5 +5,4 @@ public final class FlightMapKeys {
   private FlightMapKeys() {}
 
   public static final String TRACKED_RESOURCE = "trackedResource";
-  public static final String RESOURCE_CLEANUP_STEP = "resourceCleanupStep";
 }
