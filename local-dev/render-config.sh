b6b41a6c8ebf6d8c35fbaebcca3d32563d2c001a
yonghao <yyhao1@gmail.com>
2020-08-18 11:24:25 -0400

    fix test

--- local-dev/render-config.sh
+++ local-dev/render-config.sh
@@ -8,9 +8,9 @@ DSDE_TOOLBOX_DOCKER_IMAGE=broadinstitute/dsde-toolbox:consul-0.20.0
 VAULT_SERVICE_ACCOUNT_PATH=secret/dsde/terra/kernel/integration/toolsalpha/crl_janitor/app-sa
 VAULT_CLIENT_SERVICE_ACCOUNT_PATH=secret/dsde/terra/kernel/integration/toolsalpha/crl_janitor/client-sa
 VAULT_TOOLS_CLIENT_SERVICE_ACCOUNT_PATH=secret/dsde/terra/kernel/integration/tools/crl_janitor/client-sa
-SERVICE_ACCOUNT_OUTPUT_FILE_PATH="$(dirname $0)"/src/test/resources/rendered/sa-account.json
-CLIENT_SERVICE_ACCOUNT_OUTPUT_FILE_PATH="$(dirname $0)"/src/test/resources/rendered/client-sa-account.json
-TOOLS_CLIENT_SERVICE_ACCOUNT_OUTPUT_FILE_PATH="$(dirname $0)"/src/test/resources/rendered/tools-client-sa-account.json
+SERVICE_ACCOUNT_OUTPUT_FILE_PATH="$(dirname $0)"../src/test/resources/rendered/sa-account.json
+CLIENT_SERVICE_ACCOUNT_OUTPUT_FILE_PATH="$(dirname $0)"../src/test/resources/rendered/client-sa-account.json
+TOOLS_CLIENT_SERVICE_ACCOUNT_OUTPUT_FILE_PATH="$(dirname $0)"../src/test/resources/rendered/tools-client-sa-account.json
 
 docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN ${DSDE_TOOLBOX_DOCKER_IMAGE} \
             vault read -format json ${VAULT_SERVICE_ACCOUNT_PATH} \
