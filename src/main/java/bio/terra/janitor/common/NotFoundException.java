285e8f35d97a3d68245483872b6437ec03cd0a0f
yonghao <yyhao1@gmail.com>
2020-08-28 11:29:30 -0400

    support abandon or bump resource

--- src/main/java/bio/terra/janitor/common/NotFoundException.java
+++ src/main/java/bio/terra/janitor/common/NotFoundException.java
@@ -3,7 +3,7 @@ package bio.terra.janitor.common;
 import bio.terra.janitor.common.exception.ErrorReportException;
 import org.springframework.http.HttpStatus;
 
-/** Exception when resource met is not found. */
+/** Exception when resource met criteria is not found. */
 public class NotFoundException extends ErrorReportException {
   private static final HttpStatus thisStatus = HttpStatus.NOT_FOUND;
 
