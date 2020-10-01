844291aad5deab699da007fc1be4871fa7bd798d
wchamber <1499870+wchamber@users.noreply.github.com>
2020-10-01 14:18:36 -0400

    Add transactions for bump & abandon (#75)

--- src/main/java/bio/terra/janitor/service/janitor/TrackedResourceService.java
+++ src/main/java/bio/terra/janitor/service/janitor/TrackedResourceService.java
@@ -44,6 +44,9 @@ public class TrackedResourceService {
    * Create a new {@link TrackedResource} and update other resources with the same {@link
    * CloudResourceUid} as duplicated as appropriate. Returns the created resources {@link
    * TrackedResourceId}. This should be done as a part of a single database transaction.
+   *
+   * <p>Several parts of the Janitor system assume that there is at most one non-DONE, non-DUPLICATE
+   * TrackedResource per CloudResourceUid. Change with care.
    */
   private TrackedResourceId createResourceAndUpdateDuplicates(
       CreateResourceRequestBody body, TransactionStatus unused) {
@@ -83,6 +86,8 @@ public class TrackedResourceService {
         for (TrackedResource duplicateResource : duplicateResources) {
           janitorDao.updateResourceState(
               duplicateResource.trackedResourceId(), TrackedResourceState.DUPLICATED);
+          logger.info(
+              "Duplicated resource, trackedResourceId: {}", duplicateResource.trackedResourceId());
         }
       } else {
         // There is a duplicating resource with a later or equal expiration time. The new
@@ -121,35 +126,77 @@ public class TrackedResourceService {
     return resourceList;
   }
 
-  /** Updates the READY resource state to ABANDONED. */
+  /**
+   * Updates the READY resource state to ABANDONED.
+   *
+   * <p>Throws {@link NotFoundException} if there were no resources to abandon.
+   */
   public void abandonResource(CloudResourceUid cloudResourceUid) {
+    List<TrackedResource> abandonedResources =
+        transactionTemplate.execute(status -> abandonResourceTransaction(cloudResourceUid, status));
+    // Log only after the transaction has completed successfully so that we don't log something that
+    // got rolled back.
+    abandonedResources.forEach(
+        resource ->
+            logger.info("Abandoned resource, trackedResourceId: {}", resource.trackedResourceId()));
+  }
+
+  private List<TrackedResource> abandonResourceTransaction(
+      CloudResourceUid cloudResourceUid, TransactionStatus unused) {
     List<TrackedResource> resources =
         getResourceWithState(
-            cloudResourceUid, TrackedResourceState.READY, TrackedResourceState.CLEANING);
+            cloudResourceUid,
+            TrackedResourceState.READY,
+            TrackedResourceState.CLEANING,
+            TrackedResourceState.ERROR);
 
     if (resources.size() > 1) {
       logger.error(
-          "More than one READY or CLEANING state resources are found for resource {}.",
+          "More than one READY, CLEANING, or ERROR state resources are found during abandon for"
+              + " resource {}.",
           cloudResourceUid);
     }
-    resources.forEach(this::abandonResources);
+    resources.forEach(
+        resource ->
+            janitorDao.updateResourceState(
+                resource.trackedResourceId(), TrackedResourceState.ABANDONED));
+    return resources;
   }
 
   /**
    * Updates the ABANDONED/ERROR resource state to READY.
    *
-   * <p>It is possible that there might be multiple ABANDONED resources, and we only need to update
-   * the one with last expiration time.
+   * <p>Throws {@link NotFoundException} if there were no resources to update.
+   *
+   * <p>There should be at most one ABANDONED/ERROR resources - the others should have been
+   * DUPLICATED.
    */
   public void bumpResource(CloudResourceUid cloudResourceUid) {
+    TrackedResourceId bumpedId =
+        transactionTemplate.execute(status -> bumpResourceTransaction(cloudResourceUid, status));
+    // Log only after the transaction has completed successfully so that we don't log something that
+    // got rolled back.
+    logger.info("Bump resource, trackedResourceId: {} ", bumpedId);
+  }
+
+  private TrackedResourceId bumpResourceTransaction(
+      CloudResourceUid cloudResourceUid, TransactionStatus unused) {
     List<TrackedResource> resources =
         getResourceWithState(
             cloudResourceUid, TrackedResourceState.ABANDONED, TrackedResourceState.ERROR);
 
-    TrackedResource latestResource =
+    if (resources.size() > 1) {
+      logger.error(
+          "More than one ABANDONED or ERROR state resources are found during bump for"
+              + " resource {}.",
+          cloudResourceUid);
+    }
+
+    // Pick the latest expiration, though there should have only been one resource.
+    TrackedResource toBump =
         resources.stream().max(Comparator.comparing(TrackedResource::expiration)).get();
-    janitorDao.updateResourceState(latestResource.trackedResourceId(), TrackedResourceState.READY);
-    logger.info("Bump resource, trackedResourceId: {} ", latestResource.trackedResourceId());
+    janitorDao.updateResourceState(toBump.trackedResourceId(), TrackedResourceState.READY);
+    return toBump.trackedResourceId();
   }
 
   /**
@@ -182,11 +229,5 @@ public class TrackedResourceService {
         .creation(OffsetDateTime.ofInstant(resource.creation(), ZoneOffset.UTC))
         .expiration(OffsetDateTime.ofInstant(resource.expiration(), ZoneOffset.UTC))
         .labels(resourceAndLabels.labels());
-  }
-
-  private void abandonResources(TrackedResource trackedResource) {
-    janitorDao.updateResourceState(
-        trackedResource.trackedResourceId(), TrackedResourceState.ABANDONED);
-    logger.info("Abandoned resource, trackedResourceId: {}", trackedResource.trackedResourceId());
   }
 }
