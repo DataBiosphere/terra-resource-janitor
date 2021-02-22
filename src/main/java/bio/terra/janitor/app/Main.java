c605a198ba3ef3a5237837a811a25366f147cc36
ddietterich <ddietterich@verily.com>
2021-02-22 16:18:03 -0500

    Test minimum change

--- src/main/java/bio/terra/janitor/app/Main.java
+++ src/main/java/bio/terra/janitor/app/Main.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@ComponentScan(basePackages = "bio.terra.janitor")
+@ComponentScan(basePackages = {"bio.terra.janitor", "bio.terra.common.migrate"})
 public class Main {
   public static void main(String[] args) {
     SpringApplication.run(Main.class, args);
