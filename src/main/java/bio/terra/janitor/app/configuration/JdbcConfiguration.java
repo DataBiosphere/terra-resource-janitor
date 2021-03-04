331e57c8dcc7dd07471fab7d7a916f3b61ba64a4
John Czerkowicz <jczerk@verily.com>
2021-03-04 15:53:22 -0500

    [PF-485] Registering connection pool with JMX leads to leaks when @DirtiesContext is used, so disable in tests.

--- src/main/java/bio/terra/janitor/app/configuration/JdbcConfiguration.java
+++ src/main/java/bio/terra/janitor/app/configuration/JdbcConfiguration.java
@@ -10,15 +10,37 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import org.apache.commons.pool2.ObjectPool;
 import org.apache.commons.pool2.impl.GenericObjectPool;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 
 /** Base class for accessing JDBC configuration properties. */
 public class JdbcConfiguration {
+  private boolean jmxEnabled = true;
   private String uri;
   private String username;
   private String password;
 
   // Not a property
   private PoolingDataSource<PoolableConnection> dataSource;
+
+  /**
+   * Returns a boolean indicating whether JMX should be enabled when creating connection pools. If
+   * this is enabled, references to connection pools created at context initialization time will be
+   * placed into an MBean scoped for the lifetime of the JVM. This is OK in production, but can be
+   * an issue for tests that use @DirtiesContext and thus repeatedly initialize connection pools, as
+   * these pools will hold active DB connections while held by JMX. Having this configuration allows
+   * us to disable JMX in test environments. See PF-485 for more details.
+   */
+  public boolean isJmxEnabled() {
+    return jmxEnabled;
+  }
+
+  /**
+   * Sets the value for {@link #isJmxEnabled()}. Used by @ConfigurationProperties in extending
+   * classes ({@link JanitorJdbcConfiguration} and {@link StairwayJdbcConfiguration}).
+   */
+  public void setJmxEnabled(boolean jmxEnabled) {
+    this.jmxEnabled = jmxEnabled;
+  }
 
   public String getUri() {
     return uri;
@@ -65,8 +87,11 @@ public class JdbcConfiguration {
     PoolableConnectionFactory poolableConnectionFactory =
         new PoolableConnectionFactory(connectionFactory, null);
 
+    GenericObjectPoolConfig<PoolableConnection> config = new GenericObjectPoolConfig<>();
+    config.setJmxEnabled(isJmxEnabled());
+
     ObjectPool<PoolableConnection> connectionPool =
-        new GenericObjectPool<>(poolableConnectionFactory);
+        new GenericObjectPool<>(poolableConnectionFactory, config);
 
     poolableConnectionFactory.setPool(connectionPool);
 
