e8237bd8ed0e9a523c5a13167f395d46b1e2c9d5
zloery <6668057+zloery@users.noreply.github.com>
2022-05-06 15:29:21 -0400

    reuse wsm http client, handle errors

--- src/main/java/bio/terra/janitor/service/workspace/WorkspaceManagerService.java
+++ src/main/java/bio/terra/janitor/service/workspace/WorkspaceManagerService.java
@@ -1,12 +1,15 @@
 package bio.terra.janitor.service.workspace;
 
 import bio.terra.janitor.app.configuration.WorkspaceManagerConfiguration;
+import bio.terra.janitor.common.exception.InvalidResourceUidException;
 import bio.terra.janitor.service.iam.IamService;
 import bio.terra.workspace.api.WorkspaceApi;
 import bio.terra.workspace.client.ApiClient;
 import bio.terra.workspace.client.ApiException;
 import com.google.auth.oauth2.AccessToken;
+import java.util.Optional;
 import java.util.UUID;
+import javax.ws.rs.client.Client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,28 +17,41 @@ import org.springframework.stereotype.Component;
 @Component
 public class WorkspaceManagerService {
 
-  private WorkspaceManagerConfiguration workspaceManagerConfiguration;
-  private IamService iamService;
+  private final WorkspaceManagerConfiguration workspaceManagerConfiguration;
+  private final IamService iamService;
   // Client objects are heavyweight, so we reuse a single client despite calling multiple WSM
-  // instances
-  private final ApiClient apiClient;
+  // instances.
+  private final Client commonHttpClient;
 
   @Autowired
   public WorkspaceManagerService(
       WorkspaceManagerConfiguration workspaceManagerConfiguration, IamService iamService) {
     this.workspaceManagerConfiguration = workspaceManagerConfiguration;
     this.iamService = iamService;
-    this.apiClient = new ApiClient();
+    this.commonHttpClient = new ApiClient().getHttpClient();
+  }
+
+  private ApiClient getApiClient(String accessToken, String basePath) {
+    ApiClient client = new ApiClient().setHttpClient(commonHttpClient);
+    client.setBasePath(basePath).setAccessToken(accessToken);
+    return client;
+  }
+
+  private WorkspaceApi getWorkspaceApi(String accessToken, String basePath) {
+    ApiClient apiClient = getApiClient(accessToken, basePath);
+    WorkspaceApi workspaceClient = new WorkspaceApi();
+    workspaceClient.setApiClient(apiClient);
+    return workspaceClient;
   }
 
   public void cleanupWorkspace(UUID workspaceId, String testUser, String instanceId)
       throws ApiException {
     AccessToken userAccessToken = iamService.impersonateTestUser(testUser);
-    apiClient
-        .setBasePath(workspaceManagerConfiguration.getInstances().get(instanceId))
-        .setAccessToken(userAccessToken.getTokenValue());
-    WorkspaceApi workspaceClient = new WorkspaceApi();
-    workspaceClient.setApiClient(apiClient);
+    String wsmUrl =
+        Optional.ofNullable(workspaceManagerConfiguration.getInstances().get(instanceId))
+            .orElseThrow(
+                () -> new InvalidResourceUidException("Invalid workspace instance identifier"));
+    WorkspaceApi workspaceClient = getWorkspaceApi(userAccessToken.getTokenValue(), wsmUrl);
     workspaceClient.deleteWorkspace(workspaceId);
   }
 }
