a8c0946ff429e32821886e2547b894d3e9e7a593
zloery <6668057+zloery@users.noreply.github.com>
2022-05-10 18:35:07 -0400

    configure wsm urls in Janitor instead of map

--- src/main/java/bio/terra/janitor/app/configuration/WorkspaceManagerConfiguration.java
+++ src/main/java/bio/terra/janitor/app/configuration/WorkspaceManagerConfiguration.java
@@ -1,6 +1,6 @@
 package bio.terra.janitor.app.configuration;
 
-import java.util.HashMap;
+import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -12,13 +12,13 @@ public class WorkspaceManagerConfiguration {
 
   // A map from instance IDs to Workspace Manager instance URLs, like:
   // "dev" -> "https://workspace.dsde-dev.broadinstitute.org"
-  private HashMap<String, String> instances;
+  private List<String> instances;
 
-  public HashMap<String, String> getInstances() {
+  public List<String> getInstances() {
     return instances;
   }
 
-  public void setInstances(HashMap<String, String> instances) {
+  public void setInstances(List<String> instances) {
     this.instances = instances;
   }
 }
