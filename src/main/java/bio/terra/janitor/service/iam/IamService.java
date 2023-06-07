03180d45b6a288299de9cdc7f173f8c6ad187df2
zloery <6668057+zloery@users.noreply.github.com>
2023-06-07 12:13:41 -0400

    [PF-2781] Use signed JWT to impersonate test users (#164)
    * WIP use signed JWT instead of DWD directly, hardcoded sa
    
    * comment
    
    * fix signJwt args
    
    * jwt approach w tests
    
    * spotless
    
    * cleanup
    
    * test imports

--- src/main/java/bio/terra/janitor/service/iam/IamService.java
+++ src/main/java/bio/terra/janitor/service/iam/IamService.java
@@ -2,14 +2,32 @@ package bio.terra.janitor.service.iam;
 
 import bio.terra.common.exception.UnauthorizedException;
 import bio.terra.common.iam.AuthenticatedUserRequest;
+import bio.terra.janitor.app.configuration.CrlConfiguration;
 import bio.terra.janitor.app.configuration.IamConfiguration;
 import bio.terra.janitor.common.exception.InvalidTestUserException;
 import bio.terra.janitor.common.exception.SaCredentialsMissingException;
-import com.google.auth.oauth2.AccessToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleOAuthConstants;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.iam.credentials.v1.IamCredentialsClient;
+import com.google.cloud.iam.credentials.v1.ServiceAccountName;
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.security.GeneralSecurityException;
+import java.time.Instant;
+import java.util.Collections;
 import java.util.List;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -25,11 +43,37 @@ public class IamService {
   private static final List<String> USER_IMPERSONATION_SCOPES =
       ImmutableList.of("openid", "email", "profile");
 
+  @VisibleForTesting static final String TOKEN_INFO_URL = "https://oauth2.googleapis.com/tokeninfo";
+
+  private static final String JWT_GRANT_TYPE = "urn:ietf:params:oauth:grant-type:jwt-bearer";
+
+  // Duration that credentials impersonating test users should live, in seconds.
+  private static final long IMPERSONATED_CREDENTIALS_TIMEOUT_SECONDS = 60 * 10; // 10m
+
   private final IamConfiguration iamConfiguration;
+  private final CrlConfiguration crlConfiguration;
+  private HttpClient gcpTokenClient;
+  private IamCredentialsClient iamCredentialsClient;
 
   @Autowired
-  public IamService(IamConfiguration iamConfiguration) {
+  public IamService(IamConfiguration iamConfiguration, CrlConfiguration crlConfiguration)
+      throws IOException {
     this.iamConfiguration = iamConfiguration;
+    this.crlConfiguration = crlConfiguration;
+    this.gcpTokenClient = HttpClients.createDefault();
+    // By default, the IamCredentialsClient uses application-default credentials to make calls as
+    // the Janitor service account.
+    this.iamCredentialsClient = IamCredentialsClient.create();
+  }
+
+  @VisibleForTesting
+  void setGcpTokenClientClient(HttpClient client) {
+    this.gcpTokenClient = client;
+  }
+
+  @VisibleForTesting
+  void setIamCredentialsClient(IamCredentialsClient client) {
+    this.iamCredentialsClient = client;
   }
 
   /** Check if user is an administrator. Throws {@link UnauthorizedException} if not. */
@@ -60,21 +104,96 @@ public class IamService {
   }
 
   /**
-   * Generate an access token to impersonate the provided test user. This relies on the Janitor SA's
-   * usage of Domain-Wide Delegation to impersonate users in the test domain.
+   * Generate an access token to impersonate the provided test user.
+   *
+   * <p>This relies on the Janitor SA's usage of Domain-Wide Delegation to impersonate users in the
+   * test domain. Application-default credentials (ADC) acquired via Workload Identity on GKE do not
+   * support DWD. Instead, this method builds a custom JWT with the subject claim, uses the ADC to
+   * sign the JWT via the IamCredentials API, and finally exchanges the signed JWT for an OAuth
+   * access token to call WSM.
+   *
+   * @param testUserEmail Email of the test user to impersonate. This will be validated against the
+   *     domain allowed in configuration properties
+   * @return OAuth access token authorizing the caller to impersonate the provided test user.
    */
-  public AccessToken impersonateTestUser(String testUserEmail) {
+  public String impersonateTestUser(String testUserEmail) {
     requireTestUser(testUserEmail);
     try {
-      GoogleCredentials janitorSaCredentials = GoogleCredentials.getApplicationDefault();
-      GoogleCredentials userCredentials =
-          janitorSaCredentials
-              .createScoped(USER_IMPERSONATION_SCOPES)
-              .createDelegated(testUserEmail);
-      userCredentials.refreshIfExpired();
-      return userCredentials.getAccessToken();
+      String janitorSaEmail = getAdcEmail();
+      // Per IamCredentialsClient.signJwt() documentation, the `-` wildcard character is required;
+      // replacing it with a real project ID is invalid.
+      ServiceAccountName janitorSaName = ServiceAccountName.of("-", janitorSaEmail);
+      String signedJwt =
+          iamCredentialsClient
+              .signJwt(
+                  janitorSaName,
+                  /*delegates=*/ Collections.emptyList(),
+                  buildJwtRequestBody(testUserEmail, janitorSaEmail))
+              .getSignedJwt();
+      // Google's auth library does not support exchanging a signed JWT for an access token,
+      // so this needs to be an explicit POST call to the Google token server.
+      URI fullUri =
+          new URIBuilder(GoogleOAuthConstants.TOKEN_SERVER_URL)
+              .addParameter("grant_type", JWT_GRANT_TYPE)
+              .addParameter("assertion", signedJwt)
+              .build();
+      HttpPost tokenRequest = new HttpPost(fullUri);
+      HttpResponse response = gcpTokenClient.execute(tokenRequest);
+      return JsonParser.parseString(EntityUtils.toString(response.getEntity()))
+          .getAsJsonObject()
+          .get("access_token")
+          .getAsString();
     } catch (IOException e) {
-      throw new SaCredentialsMissingException("Unable to load Janitor SA credentials", e);
+      throw new SaCredentialsMissingException(
+          "Unable to impersonate test user via Janitor SA credentials", e);
+    } catch (GeneralSecurityException e) {
+      throw new IllegalStateException("Could not validate Janitor SA credentials.");
+    } catch (URISyntaxException e) {
+      throw new IllegalStateException("Could not parse Google OAuth token server URL");
     }
+  }
+
+  /**
+   * Get the email of the application default credentials. Generally, this should be the Janitor
+   * service account. This requires a call to GCP's tokeninfo endpoint, as client libraries only
+   * support introspection for ID tokens, not the credentials generated as ADC.
+   */
+  private String getAdcEmail() throws IOException, GeneralSecurityException, URISyntaxException {
+    GoogleCredentials janitorADC =
+        crlConfiguration.getApplicationDefaultCredentials().createScoped("email");
+    janitorADC.refreshIfExpired();
+    URI tokenInfoUri =
+        new URIBuilder(TOKEN_INFO_URL)
+            .addParameter("access_token", janitorADC.getAccessToken().getTokenValue())
+            .build();
+    HttpGet tokenInfoRequest = new HttpGet(tokenInfoUri);
+    HttpResponse response = gcpTokenClient.execute(tokenInfoRequest);
+    return JsonParser.parseString(EntityUtils.toString(response.getEntity()))
+        .getAsJsonObject()
+        .get("email")
+        .getAsString();
+  }
+
+  /**
+   * Build a request to generate and sign a custom JWT. Setting the subject claim to the test user
+   * email means the returned JWT will allow Janitor to impersonate the test user.
+   *
+   * @param testUserEmail Email of the test user to impersonate
+   * @param janitorSaEmail Email of the Janitor service account. Required as a JWT claim
+   * @return A serialized JSON blob containing all JWT claims required to impersonate a user via
+   *     domain-wide-delegation.
+   */
+  private String buildJwtRequestBody(String testUserEmail, String janitorSaEmail) {
+    Instant issuedAt = Instant.now();
+    JsonObject jwtClaims = new JsonObject();
+
+    jwtClaims.addProperty("iss", janitorSaEmail);
+    jwtClaims.addProperty("iat", issuedAt.getEpochSecond());
+    jwtClaims.addProperty(
+        "exp", (issuedAt.plusSeconds(IMPERSONATED_CREDENTIALS_TIMEOUT_SECONDS)).getEpochSecond());
+    jwtClaims.addProperty("aud", GoogleOAuthConstants.TOKEN_SERVER_URL);
+    jwtClaims.addProperty("sub", testUserEmail);
+    jwtClaims.addProperty("scope", String.join(" ", USER_IMPERSONATION_SCOPES));
+    return jwtClaims.toString();
   }
 }
---
+++ src/test/java/bio/terra/janitor/service/iam/IamServiceTest.java
@@ -0,0 +1,78 @@
+package bio.terra.janitor.service.iam;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import bio.terra.janitor.app.configuration.CrlConfiguration;
+import bio.terra.janitor.app.configuration.IamConfiguration;
+import bio.terra.janitor.common.BaseUnitTest;
+import bio.terra.janitor.common.exception.InvalidTestUserException;
+import com.google.cloud.iam.credentials.v1.IamCredentialsClient;
+import com.google.cloud.iam.credentials.v1.ServiceAccountName;
+import com.google.gson.JsonObject;
+import java.io.IOException;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.HttpClient;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Answers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@AutoConfigureMockMvc
+public class IamServiceTest extends BaseUnitTest {
+
+  @Autowired IamService iamService;
+  @Autowired IamConfiguration iamConfiguration;
+
+  @MockBean(answer = Answers.RETURNS_DEEP_STUBS)
+  CrlConfiguration crlConfiguration;
+
+  HttpClient mockGcpTokenClient = mock(HttpClient.class, Mockito.RETURNS_DEEP_STUBS);
+  IamCredentialsClient mockCredentialsClient =
+      Mockito.mock(IamCredentialsClient.class, Mockito.RETURNS_DEEP_STUBS);
+
+  @BeforeEach
+  void setup() throws IOException {
+    when(crlConfiguration.getApplicationDefaultCredentials().getAccessToken().getTokenValue())
+        .thenReturn("fakeJanitorSaToken");
+
+    // Return a JSON object with both 'access_token' and 'email' fields for all tokenClient calls.
+    // This is a bit of a hack to avoid more complex mocking for each individual call.
+    JsonObject fakeResponseJson = new JsonObject();
+    fakeResponseJson.addProperty("access_token", "MY_FAKE_ACCESS_TOKEN");
+    fakeResponseJson.addProperty("email", "FAKE_JANITOR_SA_EMAIL");
+    String serializedResponse = fakeResponseJson.toString();
+    HttpEntity fakeResponse = new StringEntity(serializedResponse, ContentType.APPLICATION_JSON);
+    when(mockGcpTokenClient.execute(any()).getEntity()).thenReturn(fakeResponse);
+    iamService.setGcpTokenClientClient(mockGcpTokenClient);
+
+    when(mockCredentialsClient
+            .signJwt(eq(ServiceAccountName.of("-", "FAKE_JANITOR_SA_EMAIL")), any(), any())
+            .getSignedJwt())
+        .thenReturn("FAKE_JWT");
+    iamService.setIamCredentialsClient(mockCredentialsClient);
+  }
+
+  @Test
+  public void canImpersonateTestUser() {
+    String testUser = "fake@" + iamConfiguration.getTestUserDomain();
+    String fakeAccessToken = iamService.impersonateTestUser(testUser);
+    assertEquals("MY_FAKE_ACCESS_TOKEN", fakeAccessToken);
+  }
+
+  @Test
+  public void cannotImpersonateRealUser() {
+    String invalidEmail = "fake@invalid.domain.com";
+    assertThrows(
+        InvalidTestUserException.class, () -> iamService.impersonateTestUser(invalidEmail));
+  }
+}
