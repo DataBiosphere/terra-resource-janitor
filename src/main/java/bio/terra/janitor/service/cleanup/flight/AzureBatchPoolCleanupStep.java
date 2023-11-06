e30bcff076e83bdc359e1e1664cbb307e51f9413
Robert Title <rtitle@broadinstitute.org>
2023-11-06 10:44:43 -0500

    Remove log

--- src/main/java/bio/terra/janitor/service/cleanup/flight/AzureBatchPoolCleanupStep.java
+++ src/main/java/bio/terra/janitor/service/cleanup/flight/AzureBatchPoolCleanupStep.java
@@ -8,12 +8,9 @@ import bio.terra.janitor.generated.model.CloudResourceUid;
 import bio.terra.stairway.StepResult;
 import bio.terra.stairway.exception.RetryException;
 import com.azure.resourcemanager.batch.BatchManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /** Step to clean up an Azure Batch Pool. */
 public class AzureBatchPoolCleanupStep extends ResourceCleanupStep {
-  private static final Logger logger = LoggerFactory.getLogger(AzureBatchPoolCleanupStep.class);
   private final CrlConfiguration crlConfiguration;
 
   public AzureBatchPoolCleanupStep(CrlConfiguration crlConfiguration, JanitorDao janitorDao) {
@@ -28,7 +25,6 @@ public class AzureBatchPoolCleanupStep extends ResourceCleanupStep {
 
     BatchManager batchManager = crlConfiguration.buildBatchManager(pool.getResourceGroup());
 
-    logger.info("XXX deleting batch pool " + pool.getId());
     return AzureUtils.ignoreNotFound(() -> batchManager.pools().deleteById(pool.getId()));
   }
 }
